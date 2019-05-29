2404d8f9584bba579d9db281e48ae3f87849f72f
ferminrp <frodriguezpenelas@gmail.com>
2019-05-29 08:06:28 -0300

    api social, header copy investigacion

--- js/social.js
+++ js/social.js
@@ -1,5 +1,4 @@
-//not updated
-url = 'https://api.sheety.co/2d06d8e4-fff7-4f46-88c3-b08e01129e77'
+url = 'https://api.sheety.co/9c0fefba-4720-45ff-abdf-6379487fd926'
 
 
 //connecting to spreadsheet and then parsing the data
