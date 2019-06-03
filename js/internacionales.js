819e5e1e5b0be49cfe232d1024f2cd953ec752e7
ferminrp <frodriguezpenelas@gmail.com>
2019-06-02 21:00:43 -0300

    agrega internacionales.js

---
+++ js/internacionales.js
@@ -0,0 +1,20 @@
+//not updated
+url = 'https://api.sheety.co/2a177fbd-1f8c-4820-89de-91a7eede5ad4'
+
+
+//connecting to spreadsheet and then parsing the data
+  $.getJSON(url, function(data) {
+    //var obj = data.feed.entry;
+    var obj = data;
+    $(obj).each(function(){
+      var ong = this.ong
+      var imagen = this.imagen
+      var paginaWeb = this.paginaWeb
+      var instagram = this.instagram
+      var twitter = this.twitter
+      var linkDonaciones = this.linkDonaciones
+
+      console.log(this.title)
+      $(".featured").append('<div class="ngo-card grow"> <a href="'+ paginaWeb +'" target="_blank" rel="noopener"><div id="featured-two" class="img-container" style="background-image:url('+imagen+')!important"></div> </a><p>'+ong+'</p><div class="icon-list"> <span><a href="'+paginaWeb+'" target="_blank" rel="noopener"><i class="fas fa-globe"></i></a></span> <span><a href="'+instagram+'" target="_blank" rel="noopener"><i class="fab fa-instagram"></i></a></span> <span><a href="'+twitter+'" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a></span></div></div>')
+   });
+  });
